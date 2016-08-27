740243333156fa4a3c47011e65ae776bc8218abe
Johan Aschan <aschan.johan@gmail.com>
2016-08-27 11:23:07 +0200

    first commit

---
+++ src/main/java/com/jaitco/QueueTicketServiceApplication.java
@@ -0,0 +1,12 @@
+package com.jaitco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueTicketServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jaitco/QueueTicketServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jaitco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QueueTicketServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
