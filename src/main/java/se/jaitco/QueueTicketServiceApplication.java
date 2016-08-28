65e2da6dcfda2068f1ea1612881999021cd3f36d
Johan Aschan <aschan.johan@gmail.com>
2016-08-28 17:39:52 +0200

    format

--- src/main/java/se/jaitco/QueueTicketServiceApplication.java
+++ src/main/java/se/jaitco/QueueTicketServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QueueTicketServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QueueTicketServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QueueTicketServiceApplication.class, args);
+    }
 }
