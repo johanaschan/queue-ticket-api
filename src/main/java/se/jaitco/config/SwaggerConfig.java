e2ab3b5f51e42600f66977ce3e8cecb72e3d8609
Johan Aschan <johan.aschan@svt.se>
2016-08-31 11:00:13 +0200

    redis

--- src/main/java/se/jaitco/config/SwaggerConfig.java
+++ src/main/java/se/jaitco/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("se.jaitco.example.controller"))
+                .apis(RequestHandlerSelectors.basePackage("se.jaitco.controller"))
                 .build()
                 .apiInfo(apiInfo());
     }
