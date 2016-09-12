5c36c986709fb4bcca8dc4cf31f5c3887845bcc3
Lars Mårdberg <larsmardberg@gmail.com>
2016-09-12 15:17:11 +0200

    removed uncessecary changes

--- src/main/java/se/jaitco/queueticketapi/model/Ticket.java
+++ src/main/java/se/jaitco/queueticketapi/model/Ticket.java
@@ -1,46 +1,12 @@
 package se.jaitco.queueticketapi.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
-//@Data
-//@EqualsAndHashCode(exclude={"time"})
+@Data
 public class Ticket {
 
     private long time;
 
     private long number;
 
-
-    public long getTime() {
-        return time;
-    }
-
-    public void setTime(long time) {
-        this.time = time;
-    }
-
-    public long getNumber() {
-        return number;
-    }
-
-    public void setNumber(long number) {
-        this.number = number;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Ticket ticket = (Ticket) o;
-
-        return number == ticket.number;
-
-    }
-
-    @Override
-    public int hashCode() {
-        return (int) (number ^ (number >>> 32));
-    }
 }
