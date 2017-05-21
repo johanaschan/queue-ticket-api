eeebf2408e5a5d1d58ba4766960791dab742a607
Johan Aschan <aschan.johan@gmail.com>
2017-05-21 21:13:38 +0200

    Unit test for UnauthorizedEntryPoint

---
+++ src/test/java/se/jaitco/queueticketapi/filter/UnauthorizedEntryPointTest.java
@@ -0,0 +1,26 @@
+package se.jaitco.queueticketapi.filter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UnauthorizedEntryPointTest {
+
+    private final UnauthorizedEntryPoint classUnderTest = new UnauthorizedEntryPoint();
+
+    @Mock
+    private HttpServletResponse httpServletResponse;
+
+    @Test
+    public void testCommence() throws Exception {
+        classUnderTest.commence(null, httpServletResponse, null);
+
+        Mockito.verify(httpServletResponse).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+
+}
